d3020169d53b30a5a7f5ea25b639bcab7d029f20
Raik Paulat <raik.paulat@charite.de>
2025-11-20 16:16:28 +0100

    Update HCS08_OpenSourceBDM_Preload.cmd

--- CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/cmd/HCS08_OpenSourceBDM_Preload.cmd
+++ CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/cmd/HCS08_OpenSourceBDM_Preload.cmd
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 // Before load the commands written below will be executed
+=======
+// Before load the commands written below will be executed
+>>>>>>> ee6fccc69f14bc60ee1207c6c470af86ffbbb5ff
