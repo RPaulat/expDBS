f38c731ca09da6e94222f248d32680ffb93cb0be
Raik Paulat <raik.paulat@charite.de>
2025-11-20 16:16:47 +0100

    Update BDM_P&E_Multilink_CyclonePro_Reset.cmd

--- CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/cmd/BDM_P&E_Multilink_CyclonePro_Reset.cmd
+++ CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/cmd/BDM_P&E_Multilink_CyclonePro_Reset.cmd
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 // After reset the commands written below will be executed
+=======
+// After reset the commands written below will be executed
+>>>>>>> ee6fccc69f14bc60ee1207c6c470af86ffbbb5ff
