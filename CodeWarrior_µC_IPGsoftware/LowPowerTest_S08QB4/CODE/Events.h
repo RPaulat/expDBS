f5eda3d67d941f89dca1107137a2206ef9fc2fe8
Raik Paulat <raik.paulat@charite.de>
2025-11-20 16:14:39 +0100

    Update Events.h

--- CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/CODE/Events.h
+++ CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/CODE/Events.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** ###################################################################
 **     Filename  : Events.H
 **     Project   : LowPowerTest_S08QB4
@@ -155,3 +156,162 @@ void _100Hz_OnInterrupt(void);
 **
 ** ###################################################################
 */
+=======
+/** ###################################################################
+**     Filename  : Events.H
+**     Project   : LowPowerTest_S08QB4
+**     Processor : MC9S08QB4CTG
+**     Component : Events
+**     Version   : Driver 01.02
+**     Compiler  : CodeWarrior HCS08 C Compiler
+**     Date/Time : 08.06.2010, 14:43
+**     Abstract  :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings  :
+**     Contents  :
+**         No public methods
+**
+** ###################################################################*/
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "_130Hz.h"
+#include "drive_Reed.h"
+#include "EInt1.h"
+#include "_2750ms.h"
+#include "send.h"
+#include "_5VOnOff.h"
+#include "T5_OnOff.h"
+#include "_100Hz.h"
+#include "Bit1.h"
+
+
+void _130Hz_OnInterrupt(void);
+/*
+** ===================================================================
+**     Event       :  _130Hz_OnInterrupt (module Events)
+**
+**     Component   :  _130Hz [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void Cap1_OnCapture(void);
+/*
+** ===================================================================
+**     Event       :  Cap1_OnCapture (module Events)
+**
+**     Component   :  Cap1 [Capture]
+**     Description :
+**         This event is called on capturing of Timer/Counter actual
+**         value (only when the bean is enabled - <Enable> and the
+**         events are enabled - <EnableEvent>.This event is available
+**         only if a <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void _60us_OnInterrupt(void);
+/*
+** ===================================================================
+**     Event       :  _60us_OnInterrupt (module Events)
+**
+**     Component   :  _60us [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void Cap1_OnOverflow(void);
+/*
+** ===================================================================
+**     Event       :  Cap1_OnOverflow (module Events)
+**
+**     Component   :  Cap1 [Capture]
+**     Description :
+**         This event is called if counter overflows (only when the
+**         bean is enabled - <Enable> and the events are enabled -
+**         <EnableEvent>.This event is available only if a <interrupt
+**         service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void _2750ms_OnInterrupt(void);
+/*
+** ===================================================================
+**     Event       :  _2750ms_OnInterrupt (module Events)
+**
+**     Component   :  _2750ms [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void EInt1_OnInterrupt(void);
+/*
+** ===================================================================
+**     Event       :  EInt1_OnInterrupt (module Events)
+**
+**     Component   :  EInt1 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void _100Hz_OnInterrupt(void);
+/*
+** ===================================================================
+**     Event       :  _100Hz_OnInterrupt (module Events)
+**
+**     Component   :  _100Hz [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 3.07 [04.34]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
+>>>>>>> ee6fccc69f14bc60ee1207c6c470af86ffbbb5ff
