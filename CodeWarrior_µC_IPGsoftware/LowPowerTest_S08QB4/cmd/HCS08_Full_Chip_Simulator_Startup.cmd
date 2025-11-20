91ea71f36342b22fb78e063fcb9e4245d083dc52
Raik Paulat <raik.paulat@charite.de>
2025-11-20 16:16:36 +0100

    Update HCS08_Full_Chip_Simulator_Startup.cmd

--- CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/cmd/HCS08_Full_Chip_Simulator_Startup.cmd
+++ CodeWarrior_µC_IPGsoftware/LowPowerTest_S08QB4/cmd/HCS08_Full_Chip_Simulator_Startup.cmd
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 // At startup the commands written below will be executed
+=======
+// At startup the commands written below will be executed
+>>>>>>> ee6fccc69f14bc60ee1207c6c470af86ffbbb5ff
